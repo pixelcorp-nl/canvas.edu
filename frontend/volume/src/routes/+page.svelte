3a9cba0b5547a70bcd865e24b3314648c50ab2d1
Oswin Bult <obult@student.codam.nl>
2023-03-08 21:49:44 +0100

    websockets in the frontend

--- frontend/volume/src/routes/+page.svelte
+++ frontend/volume/src/routes/+page.svelte
@@ -5,7 +5,9 @@ import io from 'socket.io-client';
 let canvas: HTMLCanvasElement;
 
 onMount(() => {
-	const socket = io('http://api.pixels.codam.nl/canvas');
+	const socket = io('http://api.pixels.codam.nl/canvas', {
+		transports: ['websocket'],
+	});
 	const ctx = canvas.getContext("2d")!; // TODO: Error handling
 	ctx.imageSmoothingEnabled = false;
 	ctx.scale(4, 4);
