e77c9fe5ca55590ec60f3f9961a9e7414cb838ea
Oswin Bult <obult@student.codam.nl>
2023-03-19 13:53:11 +0100

    add await to sleep

--- tests/startPlayback.py
+++ tests/startPlayback.py
@@ -4,7 +4,7 @@ import time
 
 url = "http://api.pixels.codam.nl"
 # url = "http://localhost:3000"
-code = "3eb14ed1"
+code = "a02b6e74"
 url = url + f"/admin/playback?code=" + code
 headers = {'Content-Type': 'application/json',
            'x-real-ip': 'my.amazing.fake.ip'}
