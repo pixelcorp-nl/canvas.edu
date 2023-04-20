c95ca93aceac73be9b2ba7376ac3111497242b5d
Joppe Koers <joppe@joppekoers.nl>
2023-04-20 15:41:10 +0200

    Modern types

--- frontend/src/routes/api/canvas/+server.ts
+++ frontend/src/routes/api/canvas/+server.ts
@@ -3,11 +3,9 @@ import { r } from '$api/_redis'
 import { PUBLIC_CANVAS_ID } from '$env/static/public'
 /** @type {import('./$types').RequestHandler} */
 
-type cache = {
-	[key: string]: string
-}
+export type Cache = Record<string, string>
 
-let cache: cache
+let cache: Cache
 let last_request = 0
 export async function GET() {
 	if (cache && Date.now() - last_request < 1000) {
@@ -18,7 +16,7 @@ export async function GET() {
 		console.log('cache miss')
 		const data = await r.hgetall(PUBLIC_CANVAS_ID)
 		if (data) {
-			cache = data as cache
+			cache = data as Cache
 			// console.log(data);
 			last_request = Date.now()
 			return json(data)
