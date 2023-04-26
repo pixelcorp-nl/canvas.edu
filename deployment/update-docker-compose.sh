c33c033f8985d4dffbcb903b3c6b37ba22893b33
Joppe Koers <joppe@joppekoers.nl>
2023-04-26 13:11:03 +0200

    Remove orphans

--- deployment/update-docker-compose.sh
+++ deployment/update-docker-compose.sh
@@ -8,8 +8,8 @@ wget -N $URL -P $(dirname $LOCAL_FILE_PATH)
 
 if ! cmp -s $LOCAL_FILE_PATH $(basename $URL); then
 	echo "The downloaded file is different from the local file."
-	echo "Running 'docker compose up -d'..."
-	docker compose up -d
+	echo "Running 'docker compose up (...)"
+	docker compose up -d --remove-orphans
 else
 	echo "The downloaded file is the same as the local file."
 fi
