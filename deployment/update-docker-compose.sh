bbe281049ff9ff4244ed6dc9c433469af9a9c842
Joppe Koers <joppe@joppekoers.nl>
2023-04-26 13:05:14 +0200

    Update docker compose on new image pull

---
+++ deployment/update-docker-compose.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+URL="https://raw.githubusercontent.com/pixelcorp-nl/canvas.edu/main/deployment/docker-compose.yaml"
+LOCAL_FILE_PATH="/root/docker-compose.yaml"
+
+
+wget -N $URL -P $(dirname $LOCAL_FILE_PATH)
+
+if ! cmp -s $LOCAL_FILE_PATH $(basename $URL); then
+	echo "The downloaded file is different from the local file."
+	echo "Running 'docker compose up -d'..."
+	docker compose up -d
+else
+	echo "The downloaded file is the same as the local file."
+fi
