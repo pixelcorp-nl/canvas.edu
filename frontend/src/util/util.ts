bf6c5b951c2d76e9d65eef35b82fd0ab01e5574b
Joppe Koers <joppe@joppekoers.nl>
2023-05-11 21:38:19 +0200

    Remove dupe

--- frontend/src/util/util.ts
+++
@@ -1,8 +0,0 @@
-import type { PixelMap } from '$lib/sharedTypes'
-
-export function forEachPixel(pixels: PixelMap, callback: (x: number, y: number, rgba: string) => void): void {
-	for (const [key, rgba] of Object.entries(pixels)) {
-		const [x, y] = key.split(',').map(Number) as [number, number]
-		callback(x, y, rgba)
-	}
-}
