387bd91c387193eaa15fed81ec19d37ed1ab37ff
Oswin Bult <69460150+Obult@users.noreply.github.com>
2023-06-21 22:21:15 +0200

    Create js_example.js
    adding a very basic example in js (same as the python example but translated by chatGPT)

---
+++ tests/js_example.js
@@ -0,0 +1,28 @@
+const fetch = require('node-fetch');
+
+// Define the URL of the API endpoint and the request headers
+const url = "http://pixelcorp.nl/api/single";
+const headers = { 'Content-Type': 'application/json' };
+
+// Define the pixel data as a JavaScript object
+const pixel = { x: 42, y: 42, color: [0, 25, 255, 255] };
+
+// Send a POST request to the API endpoint with the pixel data as JSON
+fetch(url, {
+  method: 'POST',
+  headers: headers,
+  body: JSON.stringify(pixel)
+})
+  .then(response => {
+    // Check if the request was successful
+    if (response.ok) {
+      // If the request was successful, print a success message
+      console.log("Pixel successfully changed!");
+    } else {
+      // If the request was unsuccessful, print an error message that includes the HTTP status code
+      console.log("Failed to change pixel:", response.status);
+    }
+  })
+  .catch(error => {
+    console.log("Error:", error);
+  });
