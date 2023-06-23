09a0e55b68400241f6bf18c8f9a046aac4e4b487
Joppe Koers <joppe@joppekoers.nl>
2023-06-23 16:55:21 +0200

    Simplify types

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,23 +1,23 @@
 import type { Server } from '$lib/sharedTypes'
 import { StatsD } from './util/statsd'
-import type { AuthRequest } from 'lucia'
 // See https://kit.svelte.dev/docs/types#app
 
 declare global {
-	namespace App {
+	declare namespace Lucia {
+		type Auth = import('$lib/server/auth').Auth
+		type UserAttributes = {
+			username: string
+			password: string
+		}
+	}
+
+	declare namespace App {
 		interface Locals {
 			io: Server
 			statsd: StatsD
-			auth: AuthRequest
+			auth: import('lucia-auth').AuthRequest
 		}
 	}
 }
 
-/// <reference types="lucia" />
-declare global {
-	namespace Lucia {
-		type Auth = import('$lib/server/auth').Auth
-		type UserAttributes = object
-	}
-}
 export {}
