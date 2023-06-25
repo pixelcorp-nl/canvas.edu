6c6f404c3fe6e013074bb6b901dfaa042b591386
meesdekker.xyz <me@meesdekker.xyz>
2023-06-26 00:23:56 +0200

    🎨 style(layout.svelte): remove footer blockquote and move github link to the image 🎨 style(page.svelte): add contributors section with their avatars and github links

---
+++ frontend/src/routes/(protected)/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { _ } from 'svelte-i18n'
+</script>
+
+<slot />
+
+<footer id="footer" class="mx-auto mt-5">
+	<blockquote>
+		{$_('footer')} <a class="person" href="https://github.com/Obult">Oswin</a>, <a class="person" href="https://github.com/LithiumOx">Mees</a> &
+		<a class="person" href="https://github.com/SirMorfield">Joppe</a>
+	</blockquote>
+</footer>
+
+<style>
+	.person:hover {
+		text-decoration: underline;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
