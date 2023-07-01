0c9b5bfca67bb5a91326f41b4a2e76591bf220c2
Joppe Koers <joppe@joppekoers.nl>
2023-07-01 23:08:56 +0200

    Make sure db is finished setting up before serving any requests

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -5,24 +5,32 @@ import type { HandleWs } from '@carlosv2/adapter-node-ws'
 import type { Handle } from '@sveltejs/kit'
 import { publicEnv } from './publicEnv'
 import { StatsD } from './util/statsd'
-import { DB, pool } from '$lib/server/db'
+import { pool } from '$lib/server/db'
 
 let listenerCount = 0
 let globalIo: Server | undefined = undefined
 const statsd = new StatsD('pixels')
 
-function setupDB() {
+let dbIsSetup = false
+async function setupDBSingleton() {
+	if (dbIsSetup) {
+		return
+	}
 	const schema = `
 CREATE TABLE IF NOT EXISTS auth_user (id TEXT PRIMARY KEY, username TEXT, apikey TEXT);
 CREATE TABLE IF NOT EXISTS auth_key (id TEXT PRIMARY KEY, user_id TEXT REFERENCES auth_user(id) NOT NULL, primary_key BOOLEAN NOT NULL, hashed_password TEXT, expires BIGINT);
 CREATE TABLE IF NOT EXISTS auth_session (id TEXT PRIMARY KEY, user_id TEXT REFERENCES auth_user(id) NOT NULL, active_expires BIGINT NOT NULL, idle_expires BIGINT NOT NULL);
 CREATE TABLE IF NOT EXISTS settings (settings text NOT NULL);
 `
-	pool.query(schema, err => {
-		if (err) {
-			throw err
-		}
+	await new Promise<void>(resolve => {
+		pool.query(schema, err => {
+			if (err) {
+				throw err
+			}
+			resolve()
+		})
 	})
+	dbIsSetup = true
 }
 
 // This file is rather weird because of a hack in adapter-node-ws
@@ -43,17 +51,16 @@ export const handleWs: HandleWs = (io: Server) => {
 		const pixels = await getPixelMap(publicEnv.canvasId)
 		socket.emit('pixelMap', pixels)
 	})
-
-	// since this call only happens once, it's fine to do it here
-	// maybe it's a weird place, but it does work
-	setupDB()
 }
 
 // Injecting global variables into the event object
-export const handle: Handle = ({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.io = globalIo as Server
 	event.locals.statsd = statsd
 	event.locals.auth = auth.handleRequest(event)
+
+	// make sure the database is setup
+	await setupDBSingleton()
 
 	return resolve(event)
 }
