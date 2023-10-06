d22ef0a0c55ca0a06fd2dfcb36d97e63f3376c01
SirMorfield <joppe@joppekoers.nl>
2023-10-06 16:52:43 +0200

    Faster test

--- frontend/tests/load.spec.ts
+++ frontend/tests/load.spec.ts
@@ -3,22 +3,17 @@ import { randomBytes } from 'crypto'
 
 test.describe.configure({ mode: 'parallel', timeout: 10_000 })
 
-const root = 'https://pixelcorp.nl'
+const root = 'https://pixels.joppekoers.nl'
 // const root = 'http://localhost:5173'
+
 async function signup(page: Page, userName: string) {
 	await page.goto(`${root}/signup`)
 	await page.waitForSelector('button[type="submit"]')
 
-	await page.evaluate(userName => {
-		;(document.querySelector('input[name="username"]') as HTMLInputElement).value = userName
-		try {
-			;(document.querySelector('#password') as HTMLInputElement).value = userName
-			;(document.querySelector('#password-confirm') as HTMLInputElement).value = userName
-		} catch (e) {
-			/**/
-		}
-		;(document.querySelector('button[type="submit"]') as HTMLButtonElement).click()
-	}, userName)
+	await page.waitForSelector('input[name="username"]')
+	await page.locator('input[name="username"]').fill(userName)
+
+	await page.click('button[type="submit"]')
 	await expect(page.locator('#header-username')).toHaveText(userName)
 	await page.goto(`${root}/info`)
 	await expect(page.locator('#footer')).toContainText('Oswin, Mees & Joppe')
@@ -30,6 +25,6 @@ function signupRand(page: Page) {
 	return signup(page, `joppe-loadtest-${randomBytes(10).toString('hex')}`)
 }
 
-for (let i = 0; i < 500; i++) {
+for (let i = 0; i < 100; i++) {
 	test(`Load ${i}`, ({ page }) => signupRand(page))
 }
