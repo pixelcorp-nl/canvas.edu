6e1e828390347ee158105a60e7e9e951adbd8d4b
SirMorfield <joppe@joppekoers.nl>
2023-10-09 19:22:30 +0200

    Update example

--- tests/update-pixel.py
+++ tests/update-pixel.py
@@ -3,11 +3,11 @@ import json
 import time
 from random import *
 import threading
+from time import sleep
 
-# url = "http://pixels.codam.nl/api/single"
+# url = "https://pixelcorp.nl/api/single"
 url = "http://localhost:5173/api/single"
-headers = {'Content-Type': 'application/json',
-		   'x-real-ip': 'my.amazing.fake.ip'}
+headers = {'Content-Type': 'application/json',}
 
 data = {
 	"x": 10,
@@ -15,9 +15,8 @@ data = {
 	"color": [255, 0, 0],
 	"key": "joppe"
 }
-response = requests.post(url, headers=headers, data=json.dumps(data))
+while True:
+	response = requests.post(url, headers=headers, data=json.dumps(data), timeout=3)
 
-if response.status_code == 200:
-	print("Send", data['x'], data['y'], data['color'], "successfully")
-else:
-	print("Failed to send", response.status_code, response.text)
+	print (response.content)
+	sleep(0.5)
