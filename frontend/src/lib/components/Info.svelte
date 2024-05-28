4ed73ceb57cd320e6ee971802be08e8fc429bb5a
meesdekker.xyz <me@meesdekker.xyz>
2024-05-28 23:12:56 +0200

    personal changes

--- frontend/src/lib/components/Info.svelte
+++ frontend/src/lib/components/Info.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
+	import { Info } from 'lucide-svelte'
 	export let content: string
 </script>
 
-<p class="bg-blue-100 p-2 rounded-lg">
+<div class="bg-blue-100 p-2 rounded-lg flex hover:shadow-sm transition-all">
+	<div class="flex items-center">
+		<Info class="w-8 h-8 stroke-blue-500" />
+	</div>
+	<span class="bg-blue text-blue-500 font-semibold my-auto ml-1.5">Info:</span>
+	<span class="my-auto mx-2 bg-white/50 p-2 rounded-md w-full">
+		{content}
+	</span>
+</div>
+
+<!-- <p class="bg-blue-100 p-2 rounded-lg">
 	<span class="bg-blue text-blue-500 font-semibold">ℹ️ Info:</span><br />
 	{content}
-</p>
+</p> -->
--- frontend/src/routes/(protected)/info/+page.svelte
+++ frontend/src/routes/(protected)/info/+page.svelte
@@ -1,5 +1,6 @@
 <!-- Script -->
 <script lang="ts">
+	export const csr = true
 	import { _ } from 'svelte-i18n'
 	import atomOneDark from 'svelte-highlight/styles/github-dark'
 	import { page } from '$app/stores'
@@ -14,6 +15,27 @@
 	const root = `${$page.url.protocol}//${$page.url.host}`
 
 	const location = `(${x}:${y})`
+	const code = `import requests
+import json
+
+${$_('comments.one')}
+url = '${root}/api/single'
+headers = {'Content-Type': 'application/json'}
+
+${$_('comments.two')}
+pixel = {'x': ${x}, 'y': ${y}, 'color': [0, 25, 255], 'key': '${apikey}' }
+
+${$_('comments.three')}
+response = requests.post(url, headers=headers, data=json.dumps(pixel))
+
+${$_('comments.four')}
+if response.ok:
+	${$_('comments.five')}
+    print("Success!", response.content)
+else:
+	${$_('comments.six')}
+    print("Error!", response.content)
+`
 	const code2 = `# Pixel informatie in JSON formaat.
 # Deze pixel wordt rood, omdat de eerste waarde van "color" helemaal aan staat en de rest helemaal uit staat.
 pixel = {'x': ${x}, 'y': ${y}, 'color': [255, 0, 0], 'key': '${apikey}' }`
@@ -27,18 +49,17 @@ pixel = {'x': ${x}, 'y': ${y}, 'color': [255, 0, 0], 'key': '${apikey}' }`
 	{@html atomOneDark}
 </svelte:head>
 
-<!-- <pre class="code">
-	{JSON.stringify(data.user, null, 2)}
-</pre> -->
-
 <!-- HTML -->
-<article class="text-column prose-md lg:prose-lg mx-auto mb-5 mt-10">
+<article class="text-column prose prose-md lg:prose-lg mx-auto mb-5 mt-10">
 	<h2 id="over-dit-project">{$_('info.title')}</h2>
 	<p><em>{$_('info.short')}</em></p>
 	<h2 id="inleiding">{$_('info.header1')}</h2>
 	<p>{$_('info.content1')}</p>
-	<Info content= {$_('info.info1')} />
-	<p>{$_('info.codeEnvironment')} <a href="https://trinket.io/embed/python3" target="_blank" class="text-white bg-orange-700 hover:bg-red-800 focus:ring-4 focus:ring-orang-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-orange-600 dark:hover:bg-orange-700 focus:outline-none dark:focus:ring-orange-800">Trinket</a></p>
+	<Info content={$_('info.info1')} />
+	<div class="rounded-lg bg-white p-4 flex justify-between">
+		{$_('info.codeEnvironment')}
+		<a href="https://trinket.io/embed/python3" target="_blank" class="">Trinket</a>
+	</div>
 
 	<!-- Basics -->
 	<Codeblock code={data.comment} />
@@ -58,9 +79,9 @@ pixel = {'x': ${x}, 'y': ${y}, 'color': [255, 0, 0], 'key': '${apikey}' }`
 	<!-- First pixel -->
 	<h3 id="color">{$_('info.header2')}</h3>
 	<p>{$_('info.putPixel')}</p>
-	<Codeblock code={$_('code.python.putPixel')} />
+	<Codeblock {code} />
 	<Opdracht content={$_('exercise.putPixel')} />
-	<Opdracht content="{$_('exercise.putPixel2')} {location} "/>
+	<Opdracht content="{$_('exercise.putPixel2')} {location} " />
 
 	<!-- Colors -->
 	<h3 id="een-wijziging-aanbrengen-op-het-canvas">{$_('header.color')}</h3>
@@ -81,21 +102,21 @@ pixel = {'x': ${x}, 'y': ${y}, 'color': [255, 0, 0], 'key': '${apikey}' }`
 	<p>{$_('info.loops1')}</p>
 	<p>{$_('info.loops2')}</p>
 	<Codeblock code={data.loop} />
-	<Info content="{$_('info.indentation')}" />
-	<Opdracht content="{$_('exercise.loop')}" />
+	<Info content={$_('info.indentation')} />
+	<Opdracht content={$_('exercise.loop')} />
 	<p>{$_('info.functions1')}</p>
 	<Codeblock code={data.func} />
 	<p>{$_('info.functions2')}</p>
-	<Opdracht content="{$_('exercise.functions1')}" />
+	<Opdracht content={$_('exercise.functions1')} />
 	<Codeblock code={data.prototype} />
-	<Opdracht content="{$_('exercise.functions2')}" />
-	<Opdracht content="{$_('exercise.functions3')}" />
+	<Opdracht content={$_('exercise.functions2')} />
+	<Opdracht content={$_('exercise.functions3')} />
 
 	<!-- Extra -->
 	<h3>{$_('header.extra')}</h3>
 
 	<p>{$_('info.extra1')}</p>
-	<Opdracht content="{$_('exercise.extra1')}" />
-	<Opdracht content="{$_('exercise.extra2')}" />
-	<Info content="{$_('info.challenges')}" />
+	<Opdracht content={$_('exercise.extra1')} />
+	<Opdracht content={$_('exercise.extra2')} />
+	<Info content={$_('info.challenges')} />
 </article>
