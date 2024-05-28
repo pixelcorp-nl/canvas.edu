3caf6ef93db5af52bb650b13987b9a7ed1e438d5
Oswin Bult <oswin@pixelcorp.nl>
2024-05-28 22:08:21 +0200

    mees do your thing

---
+++ frontend/src/lib/components/Info.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let content: string
+</script>
+
+<p class="bg-blue-100 p-2 rounded-lg">
+	<span class="bg-blue text-blue-500 font-semibold">ℹ️ Info:</span><br />
+	{content}
+</p>
