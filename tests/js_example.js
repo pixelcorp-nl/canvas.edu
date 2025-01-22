b5a00ecb12c300a1007652d48495e3e18c8b9d31
Joppe Koers <joppe@joppekoers.nl>
2025-01-22 14:32:52 +0100

    Cleanup

--- tests/js_example.js
+++ tests/js_example.js
@@ -1,28 +1,14 @@
 // Run with node js_example.js
 
-// Define the URL of the API endpoint and the request headers
-const url = "http://pixelcorp.nl/api/single";
+const url = 'https://pixelcorp.nl/api/single';
 const headers = { 'Content-Type': 'application/json' };
 
-// Define the pixel data as a JavaScript object
-const pixel = { x: 42, y: 42, color: [255, 25, 0] };
+const pixel = { x: 42, y: 42, color: [255, 25, 0], key: 'joppe' };
 
-// Send a POST request to the API endpoint with the pixel data as JSON
 fetch(url, {
-  method: 'POST',
-  headers: headers,
-  body: JSON.stringify(pixel)
-})
-  .then(response => {
-    // Check if the request was successful
-    if (response.ok) {
-      // If the request was successful, print a success message
-      console.log("Pixel successfully changed!");
-    } else {
-      // If the request was unsuccessful, print an error message that includes the HTTP status code
-      console.log("Failed to change pixel:", response.status);
-    }
-  })
-  .catch(error => {
-    console.log("Error:", error);
-  });
+	method: 'POST',
+	headers: headers,
+	body: JSON.stringify(pixel),
+}).then((response) => {
+	response.text().then((text) => console.log(text));
+});
